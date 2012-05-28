4acb5bb82561483fffd3e0a839c93a61d7ccf7c4
Marko Vidberg <marko.vidberg@remote-learner.net>
2012-05-28 15:17:38 -0400

    PDM-201 adding code from old repository

---
+++ lang/en/block_openid.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * OpenID language file for Moodle
+ *
+ * This file contains the strings used by the OpenID block. 
+ *
+ * @author Brent Boghosian <brent.boghosian@remote-learner.net>
+ * @copyright Copyright (c) 2011 Remote-Learner
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package openid
+ */
+
+// Block strings
+$string['pluginname'] = 'OpenID';
+$string['append_text'] = 'You can add another OpenID to your account by entering another OpenID here';
+$string['change_text'] = 'You can change your account to OpenID by entering your OpenID here';
